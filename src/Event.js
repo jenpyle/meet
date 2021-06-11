37cb7d9f01e325127d18d9e295a9d05387b2c304
jenpyle <jennypyle440@gmail.com>
2021-06-10 20:09:41 -0600

    Add testing and code for 'Show/Hide Details' Feature

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    showDetails: false,
+    buttonLabel: 'Show Details',
+  };
+
+  toggleShowDetails = () => {
+    this.setState({
+      showDetails: !this.state.showDetails,
+      buttonLabel: 'Show Details',
+    });
+
+    if (this.state.showDetails) {
+      this.setState({
+        buttonLabel: 'Hide Details',
+      });
+    }
+  };
   render() {
-    return <div></div>;
+    const { eventData } = this.props;
+    const { showDetails, buttonLabel } = this.state;
+    return (
+      <div className="Event">
+        <h2 className="title">{eventData.summary}</h2>
+        <p className="details">
+          `Start: ${eventData.start.dateTime}, End: ${eventData.end.dateTime}, TimeZone: ${eventData.start.timeZone}`
+        </p>
+        <button className="detailsButton" onClick={this.toggleShowDetails}>
+          {buttonLabel}
+        </button>
+        {showDetails ? (
+          <div className="moreDetails">
+            `About Event\n ${eventData.htmlLink} <br></br> ${eventData.description} Contact: $
+            {eventData.organizer.email}`
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 export default Event;
--- src/EventList.js
+++ src/EventList.js
@@ -8,7 +8,7 @@ class EventList extends Component {
       <ul className="EventList">
         {events.map((event) => (
           <li key={event.id}>
-            <Event event={event} />
+            <Event eventData={event} />
           </li>
         ))}
       </ul>
