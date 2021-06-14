67133f8dfa349e243baf67175a581ac3c7c5e7ba
jenpyle <jennypyle440@gmail.com>
2021-06-14 09:57:23 -0600

    Added showDetails without Bootstrap Accordion

--- src/Event.js
+++ src/Event.js
@@ -39,29 +39,6 @@ class Event extends Component {
                 <div> End: {eventData.end.dateTime}</div>
                 <div>TimeZone: {eventData.start.timeZone}</div>
               </p>
-              {/* <Accordion defaultActiveKey="0">
-                <Card>
-                  <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                      Click me!
-                    </Accordion.Toggle>
-                  </Card.Header>
-                  <Accordion.Collapse eventKey="0">
-                    <Card.Body>Hello! I'm the body</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-                <Card>
-                  <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                      Click me!
-                    </Accordion.Toggle>
-                  </Card.Header>
-                  <Accordion.Collapse eventKey="1">
-                    <Card.Body>Hello! I'm another body</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              </Accordion> */}
-
               <Button variant="primary" className="detailsButton" onClick={this.toggleshowingDetails}>
                 {buttonLabel}
               </Button>
