f9c587941f95584040a85904990192b65ea4b75a
jenpyle <jennypyle440@gmail.com>
2021-06-21 23:46:32 -0600

    start EndToEnd test for filter events by city feature

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -1,4 +1,43 @@
 import puppeteer from 'puppeteer';
+import { mockData } from '../mock-data';
+
+describe('Filter events by city', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    // browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.CitySearch');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test('When user hasn’t searched for a city, show upcoming events from all cities.', async () => {
+    let eventList = await page.waitForSelector('.EventList');
+
+    expect(eventList).not.toBeNull();
+  });
+
+  test('User should see a list of suggestions when they search for a city', async () => {
+    await page.type('.city', 'Berlin', { delay: 100 }); // Types slower, like a user
+    const List = await page.$('.suggestions');
+
+    // expect(List).toHaveLength(2);
+  });
+
+  test('User can select a city from the suggested list', async () => {
+    await page.click('.suggestions .list-item');
+  });
+});
 
 describe('show/hide an event details', () => {
   let browser;
@@ -6,11 +45,11 @@ describe('show/hide an event details', () => {
 
   beforeAll(async () => {
     jest.setTimeout(30000);
-    browser = await puppeteer.launch();
-    // browser = await puppeteer.launch({
-    //   headless: false,
-    //   slowMo: 250, // slow down by 250ms
-    // });
+    // browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250, // slow down by 250ms
+    });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.Event');
