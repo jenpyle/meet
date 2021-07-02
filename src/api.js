d65b1164a2ff3c26b940919b6cc2713f17a083d9
jenpyle <jennypyle440@gmail.com>
2021-07-01 21:54:56 -0600

    Add offline capability. Install does not work

--- src/api.js
+++ src/api.js
@@ -67,7 +67,11 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
-
+  if (!navigator.onLine) {
+    const data = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];
+  }
   const token = await getAccessToken();
 
   if (token) {
