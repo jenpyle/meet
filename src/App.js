96f74b0079a840f303a54e6cbc31859a2f8180ca
jenpyle <jennypyle440@gmail.com>
2021-07-08 15:31:05 -0600

    Add new rechart chart

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import { Container, Row, Col } from 'react-bootstrap';
 import './nprogress.css';
 import WelcomeScreen from './WelcomeScreen';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -18,23 +19,11 @@ class App extends Component {
     showWelcomeScreen: undefined,
   };
 
-  // componentDidMount() {
-  //   /**
-  //  * load events when the app loads.
-  //   make the API call and save the initial data to state
-  //  */
-  //   this.mounted = true;
-  //   getEvents().then((events) => {
-  //     if (this.mounted) {
-  //       /**look at componentWillUnmount */
-  //       this.setState({
-  //         events,
-  //         locations: extractLocations(events),
-  //       });
-  //     }
-  //   });
-  // }
   async componentDidMount() {
+    /**
+   *load events when the app loads.
+    make the API call and save the initial data to state
+  */
     this.mounted = true;
     if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
       getEvents().then((events) => {
@@ -85,14 +74,27 @@ class App extends Component {
     });
   };
 
+  getData = () => {
+    /**Gets the number of events at locations
+   * uses the locations and events saved in your state from the Google Calendar API. 
+    You then map the locations and filter the events by each location to get the length 
+    of the resulting array. 
+   */
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length;
+      /**split the location at the occurrence of a comma followed by a space ", ", which will return an array */
+      const city = location.split(', ').shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   render() {
-    // if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
-    //   alert("It's a local server!");
     // if (this.state.showWelcomeScreen === undefined) {
     //   return <div className="App" />;
     // }
     let offlineAlertText = '';
-
     if (!navigator.onLine) {
       offlineAlertText = 'You are currently offline. Event list may not be current.';
     }
@@ -104,6 +106,22 @@ class App extends Component {
               <WarningAlert text={offlineAlertText} />
               <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
               <NumberOfEvents updateEventNumber={(value) => this.updateEventNumber(value)} />
+              <h4>Events in each city</h4>
+              <ResponsiveContainer height={400}>
+                <ScatterChart
+                  margin={{
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                  }}
+                >
+                  <CartesianGrid />
+                  <XAxis type="category" dataKey="city" name="city" />
+                  <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+                  <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                  <Scatter data={this.getData()} fill="#8884d8" />
+                </ScatterChart>
+              </ResponsiveContainer>
               <EventList events={this.state.events} number={this.state.number} />
               <WelcomeScreen
                 showWelcomeScreen={this.state.showWelcomeScreen}
